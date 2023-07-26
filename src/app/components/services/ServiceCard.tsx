b0133d3758fbb4ca90f46058ea067faf84797a1e
Eduardo Melo <eduardo017melo@gmail.com>
2023-07-26 13:56:17 -0300

    card

--- src/app/components/services/ServiceCard.tsx
+++ src/app/components/services/ServiceCard.tsx
@@ -5,13 +5,13 @@ import { BiCaretRight } from "react-icons/bi";
 const ServiceCard = ({key, title, content, image} : ServiceCard) => {
 
   return (
-    <div className='flex flex-col items-start justify-around gap-2 rounded-tl-[50px] rounded-br-[50px] drop-shadow-lg rounded-md bg-slate-50 h-[580px] '>
-        <div className='h-[40%]  md:rounded-tl-[50px] '>
+    <div className='flex flex-col items-start justify-around gap-2 rounded-tl-[50px] rounded-br-[50px] drop-shadow-lg rounded-md bg-slate-50 h-[640px] '>
+        <div className='h-[256px]  md:rounded-tl-[50px] '>
             <Image src={image} alt='service-img' className='object-cover h-full drop-shadow-md md:rounded-br-[50px] rounded-tl-[50px] rounded-md'/>
         </div>
-        <div className='px-8 border-1 flex flex-col items-start w-[80%] justify-center gap-[1rem] text-black text-md divide-y divide-red-600'>
+        <div className='px-8 border-1 flex flex-col items-start w-[86%] justify-center gap-[1rem] text-black text-md divide-y divide-red-600'>
             <h1 className='text-black text-xl font-bold'>{title}</h1>
-             <p className='pt-3'>{content}</p>
+             <p className='pt-3 w-full'>{content}</p>
         </div>
         <div className='action w-[60%] self-start mx-8 pt-3'>
               <a className="text-sky-700 font-bold tracking-wide w-full flex items-center" href="https://api.whatsapp.com/send?phone=5551997901241" target='blank'>Peça Já seu orçamento<BiCaretRight/></a>
--- src/app/components/services/Services.tsx
+++ src/app/components/services/Services.tsx
@@ -14,34 +14,34 @@ const Services = () => {
         },
         {
             title: 'Geometria',
-            content: 'O processo de balanceamento consiste em fixar a roda em uma máquina especial chamada balanceadora.',
+            content: 'refere-se à geometria da suspensão de um veículo, também conhecida como geometria de direção ou alinhamento das rodas.',
             image: Geometry
         },
         {
             title: 'Suspensão',
-            content: 'O processo de balanceamento consiste em fixar a roda em uma máquina especial chamada balanceadora.',
+            content: 'A manutenção adequada da suspensão é essencial para garantir o bom desempenho, segurança e durabilidade do veículo. A suspensão está sujeita a desgaste natural ao longo do tempo.',
             image: Suspension
         },
         {
             title: 'Embreagem',
-            content: 'O processo de balanceamento consiste em fixar a roda em uma máquina especial chamada balanceadora.',
+            content: 'Ela permite que o motor seja desacoplado da caixa de câmbio temporariamente para permitir a troca de marchas e, em seguida, ser recoplado para transmitir a potência do motor para as rodas.',
             image: Embreagem
         },
         {
             title: 'Sistema de Freios',
-            content: 'O processo de balanceamento consiste em fixar a roda em uma máquina especial chamada balanceadora. ',
+            content: 'A manutenção adequada do sistema de freios é fundamental para garantir a segurança e o bom funcionamento do veículo. Os freios são um dos sistemas mais críticos em um carro, pois são responsáveis por reduzir a velocidade e parar o veículo quando necessário',
             image: Breaks
         },
         {
             title: 'Troca de Óleos e Fluidos',
-            content: 'O processo de balanceamento consiste em fixar a roda em uma máquina especial chamada balanceadora.',
+            content: '. Os óleos e fluidos têm diversas funções vitais nos diferentes sistemas do carro, e sua manutenção adequada é fundamental para garantir o desempenho, a eficiência e a segurança do veículo. ',
             image: Oil
         },
        
     ]
 
   return (
-    <section className="services w-full flex flex-col justify-center items-center mt-18 h-fit" id="SERVIÇOS">
+    <section className="services w-full flex flex-col justify-center items-center mt-18" id="SERVIÇOS">
         <h1 className="text-3xl text-primary-red font-extrabold mt-16 mb-14">Nossos Serviços</h1>
         <span className="font-bold text-black mb-12 text-center">Primamos por Excelência e qualidade pelos melhores preços.</span>
         <div className="services__cards-container grid  lg:grid-cols-3 gap-3 max-w-[90%] md:max-w-[75%]">
